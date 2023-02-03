b3bdd4aae269534aa12352543b9683d3a2e67624
Cryption <124207931+CryptionHub@users.noreply.github.com>
2023-02-03 11:47:08 +0100

    Update Lib.lua

--- Lib.lua
+++ Lib.lua
@@ -151,7 +151,7 @@ function CryptionHub:CreateWindow(title, gameName, intro)
         ScrollBar = Color3.fromRGB(149, 149, 149),
         NotiBackground = Color3.fromRGB(0, 0, 0),
         Glow = Color3.fromRGB(69,69,207),
-        Logo = "rbxassetid://4483345998"
+        Logo = "rbxassetid://12362528321"
     }
 
     table.insert(CryptionHub, title)
